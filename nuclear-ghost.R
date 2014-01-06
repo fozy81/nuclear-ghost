9b433399397e0e472dbca958e8906333f0378227
fozy81 <fozy81@hotmail.com>
2014-01-06 21:40:12 +0000

    first draft

---
+++ nuclear-ghost.R
@@ -0,0 +1,32 @@
+library(XML)
+library(RCurl)
+# Read and parse HTML file
+doc.html = htmlTreeParse('http://www.hse.gov.uk/nuclear/quarterly-stat/2000-4.htm',
+                         useInternal = TRUE)
+
+
+
+# Extract all the paragraphs (HTML tag is p, starting at
+# the root of the document). Unlist flattens the list to
+# create a character vector.
+doc.text = unlist(xpathApply(doc.html, '//h3', xmlValue))
+
+# Replace all \n by spaces
+doc.text = gsub('\\n', ' ', doc.text)
+
+# Join all the elements of the character vector into a single
+# character string, separated by spaces
+doc.text = paste(doc.text, collapse = ' ')
+
+
+webpage <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2010-4.htm")
+
+webpage2 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2000-4.htm")
+
+grep("h3", doc, value=TRUE)
+
+sub("<h3\b[^>]*>(.*?)</h3>","\\1", webpage2)
+
+sub(".*?h3(.*?)</p><!.*", "\\1", webpage2)
+
+sub(".*?Level(.*?)/.../.*", "\\1", webpage)
