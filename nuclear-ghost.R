59b7567bf74b9b656b81fa536d1624eeb884035b
fozy81 <fozy81@hotmail.com>
2014-01-07 00:55:01 +0000

    more data up to 2010

--- nuclear-ghost.R
+++ nuclear-ghost.R
@@ -23,7 +23,29 @@ doc2 <- readLines("http://www.hse.gov.uk/nuclear/quarterly-stat/2010-4.htm")
 
 webpage <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2010-4.htm")
 
-webpage2 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2000-4.htm")
+webpage <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2001-1.htm")
+
+webpage1 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2000-4.htm")
+webpage2 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2001-1.htm")
+webpage3 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2001-2.htm")
+webpage4 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2001-3.htm")
+webpage5 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2001-4.htm")
+webpage6 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2002-1.htm")
+webpage7 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2002-2.htm")
+webpage7 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2002-3.htm")
+webpage8 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2002-4.htm")
+webpage9 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2004-2.htm")
+webpage9 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2005-1.htm")
+webpage10 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2005-2.htm")
+webpage11 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2005-4.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2006-3.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2006-4.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2007-2.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2008-3.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2009-2.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2010-1.htm")
+webpage12 <- getURL("http://www.hse.gov.uk/nuclear/quarterly-stat/2010-4.htm")
+
 
 grep("h2", doc2, value=TRUE)
 
