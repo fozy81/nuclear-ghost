fb4fea7e4bbfe9747e59fb8842aad2750f771990
fozy81 <fozy81@hotmail.com>
2014-01-08 19:06:56 +0000

    rename files

--- csv2geojson.R
+++ csv2geojson.R
@@ -5,6 +5,6 @@ setwd("~/nuclear-ghost")
 download.file('https://raw.github.com/fozy81/nuclear-ghost/master/nuclearIncidentsUK.csv', 'nuclearIncidentsUK.csv', method='wget')
 nData <- read.csv('nuclearIncidentsUK.csv')
 summary(nData)
-nDataSP <- SpatialPointsDataFrame(nData[,c(10, 11)], nData[,-c(10,11)])
-writeOGR(nDataSP, 'ndata.geojson', 'nDataSP', driver='GeoJSON')
+nDataSP <- SpatialPointsDataFrame(nData[,c(13, 14)], nData[,-c(13,14)])
+writeOGR(nDataSP, 'nuclearIncidentsUK.geojson', 'nDataSP', driver='GeoJSON')
 
